5ee53c151f816aac9a21d9846695746856544617
Giv-08 <chutima.p8@gmail.com>
2024-06-17 10:31:02 +0000

    added 4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Description: 'This function is to display a message using puts'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+}
