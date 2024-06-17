da381abe1c6cb5bf24d39b957eebc99f183392dc
Giv-08 <chutima.p8@gmail.com>
2024-06-17 23:48:16 +0000

    added 5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
-/*
-* main - Entry point
-* Description: This function is to display a message using printf
-* Return: Always 0 (Success)
-*/
+
+/**
+ * Function main - Entry point
+ *
+ * Description: This function is the main entry point of the program and
+ * it displays a message using printf.
+ *
+ * Return: Always returns 0 (Success).
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
 }
