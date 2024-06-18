59cfa5382f13d4a049739dfe8a761f11addd8ec4
Giv-08 <chutima.p8@gmail.com>
2024-06-18 11:05:50 +0000

    0-outchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -2,12 +2,14 @@
 
 /**
  * main - Entry point
- * Description: This function prints _putchar followed by a new line using only putchar
+ * Description: This function prints _putchar followed
+ * by a new line using only putchar
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char *str = "_putchar\n";
+
 	write(1, str, 9);
 	return (0);
 }
