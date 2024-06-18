6ff527165fc133ccc82be96db6753bb40d1beaa6
Giv-08 <chutima.p8@gmail.com>
2024-06-18 05:57:56 +0000

    added 5-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: 'This function is to display all base 10 numbers'
+* Return: Always 0 (Success)
+*/
+	int main(void)
+	{
+	int number = 0;
+
+	while (number < 10)
+	{
+	putchar(number);
+	number++;
+	}
+	putchar("\n");
+	return (0);
+}
