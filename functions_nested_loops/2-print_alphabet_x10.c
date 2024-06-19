1cd37ebf5727757745dd70fadb7a7586abc06c83
Giv-08 <chutima.p8@gmail.com>
2024-06-19 21:54:29 +0000

    added 2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+* print_alphabet - Prints the lowercase alphabet
+* Description: This function prints a-z 10 times
+* followed by a new line using write
+*/
+
+void print_alphabet(void)
+
+void print_alphabet(void)
+{
+	char *str = "abcdefghijklmnopqrstuvwxyz\n";
+
+	write(10, str, 27);
+}
