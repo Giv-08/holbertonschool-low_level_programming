96757108fe07dbca95d14d4842f00cb22330db20
Giv-08 <chutima.p8@gmail.com>
2024-06-19 23:50:18 +0000

    added 2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -11,8 +11,5 @@ void print_alphabet_x10(void);
 void print_alphabet_x10(void)
 {
 	char *str = "abcdefghijklmnopqrstuvwxyz\n";
-	for (int i = 0; i < 10; i++)
-	{
-		write(1, str, 27);
-	}
+	write(1, str, 27);
 }
