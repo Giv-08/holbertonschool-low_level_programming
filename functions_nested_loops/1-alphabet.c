ce4695fb3e248e7baff893aad12f47bb967c4287
Giv-08 <chutima.p8@gmail.com>
2024-06-19 10:30:25 +0000

    added 1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -7,7 +7,17 @@
 * Prototype: void print_alphabet(void);
 * Return: Always 0 (Success)
 */
-void print_alphabet(void);
+void print_alphabet(void)
+{
+	char letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+}
 
 int main(void)
 {
