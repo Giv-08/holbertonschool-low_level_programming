086a646c28bf81167c9cfa509bc6e118d7cbe414
Giv-08 <chutima.p8@gmail.com>
2024-06-19 04:51:19 +0000

    added 1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+/**
+* main - Entry point
+* Description: This function prints _putchar followed by a new line using write
+* Return: Always 0 (Success)
+*/
+void print_alphabet(void);
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
