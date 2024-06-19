90c9ea5dfdaa0cb457bc9685ffe01b04c1abd0fa
Giv-08 <chutima.p8@gmail.com>
2024-06-19 11:27:50 +0000

    added 1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 
 /**
-* main - Entry point
+* print_alphabet - Prints the lowercase alphabet followed by a newline
 * Description: This function prints a-z followed by a new line using write
 * Return: Always 0 (Success)
 */
