c7c179d4c6c6f3107850bc852b51e0ee06d828eb
Giv-08 <chutima.p8@gmail.com>
2024-06-20 04:44:55 +0000

    added 7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -16,12 +16,11 @@ int print_last_digit(int n)
 
 	if (last_digit < 0)
 	{
-		return (-last_digit);
+		last_digit = -last_digit;
 	}
 	else
 	{
 		return (last_digit);
 	}
-
-	return (last_digit);
+	return (0);
 }
