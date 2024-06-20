83cb52a6c4d8ac812373cb79ceb12b92f5f66e80
Giv-08 <chutima.p8@gmail.com>
2024-06-20 01:57:55 +0000

    added 4-isalpha.c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _isalpha - Checks if 'c' is a lowercase or uppercase character
+* @c: The character to check
+* Description: This function takes a single character as an input
+* and returns 1 if the character is a upper/lowercase letter
+* ('a',('A') to 'z',('Z')),
+* otherwise it returns 0
+* Return: 1 if c is lowercase, 0 otherwise
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' && c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+
+	}
+}
