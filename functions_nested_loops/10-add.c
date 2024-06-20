dc1e44489c4dce6df044a66e53e1d798841f7892
Giv-08 <chutima.p8@gmail.com>
2024-06-20 05:40:52 +0000

    aded 9-times_table.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* add - Adds two integers 'a' abd 'b' then return the result
+* @a: is an interger taken as a parameter
+* @b: is an interger taken as a parameter
+* Return: result of calculation
+*/
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+
+}
