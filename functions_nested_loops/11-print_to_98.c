9bce19998dff5bef99158154ad7724f7d44e6b6d
Giv-08 <chutima.p8@gmail.com>
2024-06-20 06:12:07 +0000

    added 11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,32 +9,32 @@
 */
 void print_to_98(int n)
 {
+	int i;
 
 	if (n < 98)
 	{
-		while (n < 98)
+		for (i = n; i <= 98; i++)
 		{
-			_putchar(n + '0');
-			_putchar(',');
-			_putchar(' ');
-			n++;
-			_putchar(98);
+			_putchar(i + '0');
+			if (i != 98)
+			{
+				_putchar(',');
+				putchar(' ');
+			}
 		}
-	}
-	else if (n == 98)
-	{
-		_putchar(n + '0');
 	}
 	else
 	{
-		while (n > 98)
+		for (i = n; i>= 98; i--)
 		{
-			_putchar(n + '0');
-			_putchar(',');
-			_putchar(' ');
-			n--;
-			_putchar(98);
+			_putchar(i + '0');
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 	}
+	_putchar('\n');
 }
 
