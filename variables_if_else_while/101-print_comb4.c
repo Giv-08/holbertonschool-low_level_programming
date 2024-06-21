7043ff3739ada96811134b5b51aa2d220b779977
Giv-08 <chutima.p8@gmail.com>
2024-06-21 04:58:37 +0000

    added 100-print_comb3.c

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -11,11 +11,11 @@ int main(void)
 {
 	int m, n, o;
 
-	for (m = 0; m < 8; m++)
+	for (m = 0; m < 10; m++)
 	{
-		for (n = m + 1; n < 9; n++)
+		for (n = m + 1; n < 10; n++)
 		{
-			for (o = n + 1; 0 < 10; o++)
+			for (o = n + 1; o < 10; o++)
 			{
 				putchar(m + '0');
 				putchar(n + '0');
