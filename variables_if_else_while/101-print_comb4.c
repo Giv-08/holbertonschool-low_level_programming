3a074a42e4d8bb5d1eb8998721c77cef2fb20e86
Giv-08 <chutima.p8@gmail.com>
2024-06-21 04:41:07 +0000

    added 100-print_comb3.c

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: 'This function displays all possible
+* combinations of three-digit numbers
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int m, n, o;
+
+	for (m = 0; m < 10; m++)
+	{
+		for (n = m + 1; n < 10; n++)
+		{
+			for (o = n + 1; 0 < 10; o++)
+			{
+				putchar(n + '0');
+				putchar(m + '0');
+				putchar(o + '0');
+			}
+		if (n != 7 || m != 8 || o != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
