f1fadc6aeb17eacda55bcb549d130b8629d6992f
Giv-08 <chutima.p8@gmail.com>
2024-06-21 05:14:21 +0000

    added 102-print_comb5.c

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: 'This function displays all possible
+* combinations of four-digit numbers
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int num_one, num_two;
+
+	for (num_one = 0; num_one < 100; num_one++)
+	{
+		for (num_two = num_one + 1; num_two < 100; num_two++)
+		{
+			putchar((num_one / 10) + '0');
+			putchar((num_one % 10) + '0');
+			putchar(' ');
+			putchar((num_two / 10) + '0');
+			putchar((num_two % 10) + '0');
+
+			if (num_one != 98 || num_two != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
