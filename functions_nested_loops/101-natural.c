04bd5d1615e27c4201ed1373b5a2f649c84ec788
Giv-08 <chutima.p8@gmail.com>
2024-06-22 10:53:12 +0000

    added 101-natural.c

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: This function lists all the natural numbers below
+* 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum
+* of these multiples is 23. Write a program that computes and prints
+* the sum of all the multiples of 3 or 5 below 1024 (excluded)
+*/
+
+int main(void)
+{
+	int sum = 0;
+
+	for (int i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += 1;
+		}
+	}
+	putchar(sum + '0');
+	return (0);
+}
