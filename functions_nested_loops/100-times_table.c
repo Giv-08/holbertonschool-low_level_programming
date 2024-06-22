9ae9914e72aa832e5c0b44b0554dc2fa02d277ff
Giv-08 <chutima.p8@gmail.com>
2024-06-22 05:06:24 +0000

    added 100-times_table

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_times_table - Prints the n times table, starting with 0
+* if n is greater than 15 or less than 0 the function should not print anything
+* followed by a new line
+* @n: is an interger taken as a parameter
+* Return: Always returns 0 (success)
+*/
+
+void print_times_table(int n)
+{
+	if (n == 0 || n > 15)
+	{
+		return;
+	}
+
+	int a, b, cal;
+
+	for (a = 0; a <= n; a++)
+	{
+		for (b = 0; b <= n; n++)
+		{
+			cal = a * b;
+
+			if (b != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				if (cal < 10)
+				{
+					_putchar(' '), _putchar(' ');
+				}
+				else if (cal < 100)
+				{
+					_putchar(' ');
+				}
+			}
+			if (cal >= 100)
+			{
+				_putchar((cal / 100) + '0');
+			}
+			else if (cal >= 10)
+			{
+				_putchar(((cal / 100) % 10) + '0');
+			}
+			_putchar((cal % 10) + '0');
+		}
+		_putchar('\n');
+
+}
