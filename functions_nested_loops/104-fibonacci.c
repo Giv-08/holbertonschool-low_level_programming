4334c5a2d9c1c0dc4eb45ab43604d942b1c52ba7
Giv-08 <chutima.p8@gmail.com>
2024-06-23 04:27:41 +0000

    added 104-fibonacci

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: This function finds and prints
+* the first 98 Fibonacci numbers, starting with 1 and 2
+* Return: Always 0 (Success)
+**/
+
+int main(void)
+{
+
+	int fib_one = 1;
+	int fib_two = 2;
+	int next;
+	int count = 0;
+
+	while (count <= 98)
+	{
+
+		next = fib_one + fib_two;
+		fib_one = fib_two;
+		fib_two = next;
+		count++;
+	}
+	return (0);
+}
