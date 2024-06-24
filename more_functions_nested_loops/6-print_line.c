cfbf4780511c9d218c8e0b328d18636375c65530
Giv-08 <chutima.p8@gmail.com>
2024-06-24 05:23:19 +0000

    added print_line.c

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -11,11 +11,11 @@ void print_line(int n)
 {
 	if (n > 0)
 	{
-		_putchar((n * "_") + '0');
+		_putchar((n + '0') * 95);
 	}
 	else if (n <= 0)
 	{
-		_putchar("\n");
+		_putchar('\n');
 	}
-	_putchar("\n");
+	_putchar('\n');
 }
