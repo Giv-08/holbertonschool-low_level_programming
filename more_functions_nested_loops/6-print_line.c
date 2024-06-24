01a5ffd572607326093898fdf1220f6662f1c7fb
Giv-08 <chutima.p8@gmail.com>
2024-06-24 05:18:11 +0000

    added print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_line - Draws a straight line in the terminal
+* followed by a new line
+* @n: Take as an input of integer
+*/
+
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		_putchar((n * '_') + '0');
+	}
+	else if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
