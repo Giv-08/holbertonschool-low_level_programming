411f9f16e0b4268de1db79182603fbc9dfb92a03
Giv-08 <chutima.p8@gmail.com>
2024-06-25 02:01:10 +0000

    added 9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,26 +10,26 @@ int main(void)
 
 	for (i = 1; i < 101; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("%s ", "FizzBuzz");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("%s ", "Buzz");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("%s ", "Fizz");
-		}
-
 		if (i % 3 != 0 && i % 5 != 0)
 		{
+			if (i % 3 == 0 && i % 5 == 0)
+			{
+				printf("%s ", "FizzBuzz");
+			}
+			else if (i % 5 == 0)
+			{
+				printf("%s ", "Buzz");
+			}
+			else if (i % 3 == 0)
+			{
+				printf("%s ", "Fizz");
+			}
+			else if (i == 100)
+			{
+				printf("%s", "FizzBuzz")
+			}
+
 			printf("%d ", i);
-		}
-		else if (i == 100)
-		{
-			printf("%d", i);
 		}
 	}
 	printf("\n");
