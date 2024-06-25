cd8b50e8141d4bf367de08d72a961671d17d3fdf
Giv-08 <chutima.p8@gmail.com>
2024-06-25 09:35:05 +0000

    added 0-reset_to_98

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,7 @@
 /**
 * reset_to_98 - Takes a pointer to an int
 * as parameter and updates the value it points to to 98.
-* @*n: Take '*n' as parameter
+* @n: Take '*n' as parameter
 */
 
 void reset_to_98(int *n)
