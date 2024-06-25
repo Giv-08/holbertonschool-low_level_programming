0b2d990e61e62fb4bf61e8a962e30f5b439c13e8
Giv-08 <chutima.p8@gmail.com>
2024-06-25 03:50:02 +0000

    added 10-print_triangle.c

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -22,13 +22,13 @@ void print_triangle(int size)
 
 		for (count = 0; count < size; count++)
 		{	
-			for (i = 1; i < size; i++)
+			for (i = 0; i < size; i++)
 			{
 				for (j = 0; j < size; j++)
 				{
-					_putchar(' ');
+					_putchar('#');
 				}
-				_putchar('#');
+				_putchar(' ');
 			}
 			_putchar('\n');
 		}
