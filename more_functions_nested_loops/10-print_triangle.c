3de05f151b281e6910d4428ca514f50959dafe91
Giv-08 <chutima.p8@gmail.com>
2024-06-25 03:24:55 +0000

    added 10-print_triangle.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_square - Prints a triangle
+* followed by a new line
+* @size: Represent "#"
+*/
+
+void print_triangle(int size)
+{
+	int count;
+	int i;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+
+		for (count = 0; count < size; count++)
+		{
+			for (i = 0; i < size; i++)
+			{
+				_putchar(' ');
+			}
+			_putchar('#');
+		}
+	}
+}
