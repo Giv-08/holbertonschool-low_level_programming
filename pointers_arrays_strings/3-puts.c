3562deb85b104725396c1572475f42a6c1a9adda
Giv-08 <chutima.p8@gmail.com>
2024-06-26 00:00:19 +0000

    added 3-puts.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _puts - Prints a string, followed by a new line, to stdout
+* @str: Take char '*str' as parameter
+*/
+
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
