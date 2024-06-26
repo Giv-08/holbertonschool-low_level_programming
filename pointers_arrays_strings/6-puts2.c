10cf9b96136813c0404e20d14f348c1038e20909
Giv-08 <chutima.p8@gmail.com>
2024-06-26 03:32:39 +0000

    added 6-puts2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* puts - prints every other character of a string, 
+* starting with the first character, followed by a new line
+*/
+ 
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		if (*str % 2 == 0)
+		{
+		_putchar(*str);
+		str++;
+		}
+	}
+}
