6baf81981fe268056c09a43117b1f349b8b73d9a
Giv-08 <chutima.p8@gmail.com>
2024-06-26 09:40:07 +0000

    added 8-print_array.c

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -20,7 +20,7 @@ void print_array(int *a, int n)
 
 		for (i = 0; i < n; i++)
 		{
-			if (i == n-1)
+			if (i == n - 1)
 			{
 				printf("%d", a[i]);
 			}
@@ -29,5 +29,6 @@ void print_array(int *a, int n)
 				printf("%d, ", a[i]);
 			}
 		}
+		printf('\n');
 }
 
