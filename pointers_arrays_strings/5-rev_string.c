7ae3721b4a9536d4b190e32a035779a730c3ebb4
Giv-08 <chutima.p8@gmail.com>
2024-06-27 23:50:24 +0000

    added 5-rev_string

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -11,15 +11,11 @@ void rev_string(char *s)
 	int i;
 	int length = 0;
 
-	while (s[length] != '\0')
-	{
 	 	for (i = length - 1; i >= 0; i--)
         	{
                		_putchar(s[i]);
         	}
-		length++;
-	}
-	
+		
 	_putchar('\n');
 
 }
