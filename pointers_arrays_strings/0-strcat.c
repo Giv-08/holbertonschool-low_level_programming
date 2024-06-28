83a3f0a6275b3e87144ac97026520eb30a418b5a
Giv-08 <chutima.p8@gmail.com>
2024-06-28 03:57:02 +0000

    added 0-strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -16,10 +16,16 @@ char *_strcat(char *dest, char *src)
 
 	while (*dest != '\0')
 	{
+		dest++;
+	}
+	printf("%s printf     ", dest);
+
+	while (*src != '\0')
+	{
 		*dest = *src;
 		src++;
 		dest++;
 	}
-
+	*dest = '\0';
 	return (original_dest);
 }
