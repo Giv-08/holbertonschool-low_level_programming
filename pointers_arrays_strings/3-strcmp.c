09669e157a55aa910c5a9d0d22b9c3e22836ae53
Giv-08 <chutima.p8@gmail.com>
2024-06-28 11:30:03 +0000

    added 3-strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,13 +9,25 @@
 
 int _strcmp(char *s1, char *s2)
 {
-        while (*s1 == *s2)
+	int i = 0;
+
+        while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (*s1 != '\0' && *s2 != '\0')
-		{
 			s1++;
 			s2++;
-		}
+	}
+
+	if (s1[i] > s2[i])
+	{
+		printf("%d\n", s1[i]);
+	}
+	else if (s1[i] < s2[i])
+	{
+		printf("%d\n", s2[i]);
+	}
+	else if (s1[i] == s2[i])
+	{
+		printf("0");
 	}
 	return (0);
 }
