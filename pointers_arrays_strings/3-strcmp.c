c8de8203a05672784f67c03864ab96a4f460d057
Giv-08 <chutima.p8@gmail.com>
2024-06-28 11:19:11 +0000

    added 3-strcmp

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* *_strncmp - Compares two strings
+* @s1: - Task char '*dest' as parameter
+* @s2: - Task char '*src' as parameter
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+        while (*s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (0);
+}
