e5af4bb7b991d8ab94d3cad9b78b8057f49a3598
Giv-08 <chutima.p8@gmail.com>
2024-06-28 11:41:19 +0000

    added 3-strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -2,32 +2,31 @@
 #include "main.h"
 
 /**
-* *_strncmp - Compares two strings
-* @s1: - Task char '*dest' as parameter
-* @s2: - Task char '*src' as parameter
+* _strcmp - Compares two strings
+* @s1: - Task char '*s1' as parameter
+* @s2: - Task char '*s2' as parameter
 */
 
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-        while (s1[i] != '\0' && s2[i] != '\0')
+	while (s1[i] == s2[i])
 	{
-			s1++;
-			s2++;
+		if (s1[i] == '\0' || s2[i] == '\0')
+		{
+			break;
+		}
+		i++;
 	}
 
-	if (s1[i] > s2[i])
+	if (s1[i] == '\0' && s2[i] == '\0')
 	{
-		printf("%d\n", s1[i]);
+		return (0);
 	}
-	else if (s1[i] < s2[i])
+	else
 	{
-		printf("%d\n", s2[i]);
+		return (s1[i] - s2[i]);
 	}
-	else if (s1[i] == s2[i])
-	{
-		printf("0");
-	}
-	return (0);
+
 }
