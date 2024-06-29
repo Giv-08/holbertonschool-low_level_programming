276fa06a7b00d05b079d12bcf270591db4d93b18
Giv-08 <chutima.p8@gmail.com>
2024-06-29 05:04:25 +0000

    added 100-atoi.c

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _atoi - convert a string to an integer
+* The number in the string can be preceded by
+* an infinite number of characters
+* You need to take into account all the - and
+* + signs before the number
+* @s: Take char 's' as character
+* Return: Always 0 (success)
+*/
+
+int _atoi(char *s)
+{
+	int text = 0;
+	int symbol = 1;
+	int i = 0;
+
+	if (s[0] == '-')
+	{
+		symbol = -1;
+		i++;
+	}
+	else if (s[0] == '+')
+	{
+		i++;
+	}
+
+	while (s[i] != '\0')
+	{
+		if (s[i] < '0' || s[i] > 9)
+		{
+			break;
+		}
+		text = text * 10 + (s[i] - '0');
+		i++;
+	}
+	return (symbol * text);
+}
+
