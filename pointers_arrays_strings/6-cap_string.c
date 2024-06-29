f774ca4423d801d4e189541bffded143d5e2153e
Giv-08 <chutima.p8@gmail.com>
2024-06-29 02:54:21 +0000

    added 6-cap_string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,29 +2,41 @@
 #include <stdio.h>
 
 /**
+* is_separator - check if a character is a word separator
+* @c: Take char 'c' as character
+* Return: all separetors
+*/
+
+int is_separator(char c)
+{
+	return (c == ' ' || c == '\t' || c == '\n' || c == ',' || c == ';' ||
+		c == '.' || c == '!' || c == '?' || c == '"' || c == '(' ||
+		c == ')' || c == '{' || c == '}');
+}
+
+/**
 * *cap_string - capitalizes all words of a string
 * @s: Take char '*s' as strings
 * Return: s as original address
 */
 
-int is_separator(char c) {
-    return (c == ' ' || c == '\t' || c == '\n' || c == ',' || c == ';' ||
-            c == '.' || c == '!' || c == '?' || c == '"' || c == '(' ||
-            c == ')' || c == '{' || c == '}');
-}
-
 char *cap_string(char *s)
 {
-        char *cap = s;
+	char *cap = s;
+	int new_word = 1;
 
-        while (*cap != '\0' && !is_separator(*s))
-        {
-                if (*cap >= 'a' && *cap <= 'z')
-                {
-                       	*cap = *cap - 32;
-                }
-                cap++;
-		break;
-        }
-        return (s);
+	while (*cap != '\0')
+	{
+		if (is_separator(*cap))
+			new_word = 1;
+		else if (new_word && *cap >= 'a' && *cap <= 'z')
+		{
+			*cap = *cap - 32;
+			new_word = 0;
+		}
+		else
+			new_word = 0;
+		cap++;
+	}
+	return (s);
 }
