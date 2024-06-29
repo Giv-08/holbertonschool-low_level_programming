b75602ad4e4de1a86a97fa9e5fb062668f01bbba
Giv-08 <chutima.p8@gmail.com>
2024-06-29 04:15:15 +0000

    added 7-leet.c

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -13,11 +13,26 @@ char *leet(char *s)
 	
 	while (*text != '\0')
 	{
-		if (*text == 'a' || *text == 'A') *text = '4';
-		if (*text == 'e' || *text == 'E') *text = '3';
-		if (*text == 'o' || *text == 'O') *text = '0';
-		if (*text == 't' || *text == 'T') *text = '7';
-		if (*text == 'l' || *text == 'L') *text = '1';
+		if (*text == 'a' || *text == 'A')
+                {
+                        *text = 4 + '0';
+                }
+                if (*text == 'e' || *text == 'E')
+                {
+                        *text = 3 + '0';
+                }
+                if (*text == 'o' || *text == 'O')
+                {
+                        *text = 0 + '0';
+                }
+                if (*text == 't' || *text == 'T')
+                {
+                        *text = 7 + '0';
+                }
+                if (*text == 'l' || *text == 'L')
+                {
+                        *text = 1 + '0';
+                }
 		
 		text++;
 	}
