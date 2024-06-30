41621a0bf1c5191c1f7398406d26eea59cdd199e
Giv-08 <chutima.p8@gmail.com>
2024-06-30 05:53:49 +0000

    added 101-print_number

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -6,28 +6,19 @@
 * @n: Take 'n' as parameter
 */
 
-void print_digits(int num)
-{
-    unsigned int digit;
-    if (num == 0) {
-        return;
-    }
-    digit = num % 10;
-    print_digits(num / 10);
-    _putchar(digit + '0');
-}
+void print_number(int n) {
+    unsigned int num;
 
-void print_number(int n)
-{
     if (n < 0) {
         _putchar('-');
-        n = -n;
+        num = -n;
+    } else {
+        num = n;
     }
 
-    if (n == 0) {
-        _putchar('0');
-        return;
+    if (num / 10) {
+        print_number(num / 10);
     }
 
-    print_digits(n);
+    _putchar((num % 10) + '0');
 }
