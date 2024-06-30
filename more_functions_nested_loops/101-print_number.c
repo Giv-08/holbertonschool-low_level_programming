c11074f6b4c4b499c95b296f847f3bf659ce0da1
Giv-08 <chutima.p8@gmail.com>
2024-06-30 05:47:53 +0000

    added 101-print_number

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_number - Prints an integer
+* @n: Take 'n' as parameter
+*/
+
+void print_digits(int num)
+{
+    int digit;
+    if (num == 0) {
+        return;
+    }
+    digit = num % 10;
+    print_digits(num / 10);
+    _putchar(digit + '0');
+}
+
+void print_number(int n)
+{
+    if (n < 0) {
+        _putchar('-');
+        n = -n;
+    }
+
+    if (n == 0) {
+        _putchar('0');
+        return;
+    }
+
+    print_digits(n);
+}
