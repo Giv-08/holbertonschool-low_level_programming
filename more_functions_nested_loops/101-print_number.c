0803299a12db9425d2375d33d4b0142e4138e676
Giv-08 <chutima.p8@gmail.com>
2024-06-30 06:01:29 +0000

    added 101-print_number

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -6,15 +6,27 @@
 * @n: Take 'n' as parameter
 */
 
-void print_number(int n) {
-    if (n < 0) {
+void print_number(int n)
+{
+	if(n/10!=0)
+    {
+        print_number(n/10);
+        if ( n > 0 )
+        {
+           _putchar(n%10 + '0');
+        }
+        else
+        {
+           _putchar(-n%10 + '0');
+        }
+    }
+    else if((n/10==0) && (n%10!=0) && (n>0))
+    {
+        _putchar(n%10 + '0');
+    }
+    else if((n/10==0) && (n%10!=0) && (n<=0))
+    {
         _putchar('-');
-        n = -n;
+        _putchar(-n%10+'0');
     }
-
-    if (n / 10 != 0) {
-        print_number(n / 10);
-    }
-    
-    _putchar(n % 10 + '0');
 }
