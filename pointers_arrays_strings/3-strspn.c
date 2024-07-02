9f772e302c6aed83ecf2592283baca490e93c5c5
Giv-08 <chutima.p8@gmail.com>
2024-07-02 04:07:23 +0000

    added 3-strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -11,33 +11,32 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
+	unsigned int a = 0;
+	unsigned int b = 0;
 	unsigned int counter = 0;
-	char *p, *a;
-	int found;
+	int loop = 1; 
+	int found = 0;
 
-	p = s;
-	a = accept;
-
-	while (*p != '\0')
-	{
+	while (s[a] != '\0' && loop == 1)
+	{	
 		found = 0;
-		while (*a != '\0')
+		b = 0;
+		while (accept[b] != '\0')
 		{
-			if (*p == *a)
+			if (s[i] == accept[b])
 			{
+				counter++;
 				found = 1;
-				break;
 			}
-			a++;
+			b++;
 		}
-		p++;
 
 		if (!found)
 		{
 			break;
 		}
 
-		counter++;
+		a++;
 	}
 	return (counter);
 
