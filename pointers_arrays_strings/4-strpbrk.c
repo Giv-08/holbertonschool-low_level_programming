9d1bb7f22444de47b006394ecf8d3e08721b330e
Giv-08 <chutima.p8@gmail.com>
2024-07-02 04:51:53 +0000

    added 4-strpbrk

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -11,33 +11,22 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int a = 0;
-        unsigned int b = 0;
-        unsigned int counter = 0;
-        int loop = 1;
-        int found = 0;
+	char *ptr;
 
-        while (s[a] != '\0' && loop == 1)
+        while (*s != '\0')
         {
-                found = 0;
-                b = 0;
-                while (accept[b] != '\0')
+                ptr = accept;
+                while (*ptr != '\0')
                 {
-                       if (s[a] == accept[b])
+                       if (*s == *ptr)
                        {
-                               counter++;
-                               found = 1;
+                               return ((char *)s);
                        }
-                       b++;
+                       ptr++;
                 }
 
-                if (!found)
-                {
-                        break;
-                }
-
-                a++;
+                s++;
         }
-        return (s);
+        return (NULL);
 
 }
