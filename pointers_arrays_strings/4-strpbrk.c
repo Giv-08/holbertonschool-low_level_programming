d4d1ae9194338970e9c092e9af275f61b54a88dd
Giv-08 <chutima.p8@gmail.com>
2024-07-02 02:43:37 +0000

    added 4-strpbrk

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -5,7 +5,7 @@
 * *_strpbrk - searches a string for any of a set of bytes
 * @s: Take char '*s' as parameter
 * @accept: Take char '*accept' as parameter
-* Return: a pointer to the byte in s that matches one of 
+* Return: a pointer to the byte in s that matches one of
 * the bytes in accept, or NULL if no such byte is found
 */
 
@@ -13,17 +13,17 @@ char *_strpbrk(char *s, char *accept)
 {
 	char *a = accept;
 
-	while (*s != '\0') 
+	while (*s != '\0')
 	{
-		while (*a != '\0') 
+		while (*a != '\0')
 		{
-			if (*s == *a) 
+			if (*s == *a)
 			{
-				return s;
+				return (s);
 			}
-			++a;
-        	}
-		++s;
+			a++;
+		}
+		s++;
 	}
-	return NULL;
+	return (NULL);
 }
