3776765361e1aec6842eb86f9e4e70556d245e82
Giv-08 <chutima.p8@gmail.com>
2024-07-04 05:11:29 +0000

    added 4-pow_recursion

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _pow_recursion - check the code
+ * @x: Take int 'x' as parameter
+ * @n: Take int 'n' as parameter
+ * Return: Always 0
+*/
+
+int _pow_recursion(int x, int y);
+{
+
+}
+
+
