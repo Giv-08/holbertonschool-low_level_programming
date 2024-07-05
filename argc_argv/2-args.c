ef682ed887805afc2b7a586d3de85134b40de212
Giv-08 <chutima.p8@gmail.com>
2024-07-05 11:29:21 +0000

    added vi 2-args.c

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -9,11 +9,11 @@
  */
 int main(int argc, char *argv[])
 {
-	int i = 1;
+	int i = 0;
 
-	while (i < argc - 1)
+	while (i < argc)
 	{
-		printf("%s\n", *argv);
+		printf("%s\n", argv[i]);
 		i++;
 	}
 	return (0);
