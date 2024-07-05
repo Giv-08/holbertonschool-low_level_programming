52066b813f72bdb9f482d0703949e7db4a9bbb85
Giv-08 <chutima.p8@gmail.com>
2024-07-05 11:06:03 +0000

    added 1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main -  prints the number of arguments passed into it
+ * @argc: Take int 'argc' as parameter
+ * @argv: Take char '*argv' as parameter
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i = 1;
+
+	while (i < argc)
+	{
+		printf("%d\n", *argv[i]);
+		i++;
+	}
+	return (0);
+}
