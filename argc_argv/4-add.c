483816518e63ed299fb41eaab6e676c1c231ea57
Giv-08 <chutima.p8@gmail.com>
2024-07-06 05:08:19 +0000

    added 4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -7,6 +7,9 @@
  * main - adds positive numbers
  * @argc: Take int 'argc' as parameter
  * @argv: Take char '*argv' as parameter
+ * Return: If one of the number contains symbols
+ * that are not digits, print Error, followed by
+ * a new line, and return 1
 */
 
 int main(int argc, char *argv[])
@@ -15,12 +18,12 @@ int main(int argc, char *argv[])
 	int b = 0;
 	int total = 0;
 
-	if (argc == 0)
+	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
 	}
-	
+
 	while (a < argc)
 	{
 		while (argv[a][b] != '\0')
@@ -32,8 +35,8 @@ int main(int argc, char *argv[])
 			}
 			b++;
 		}
-		a++;
 		total += atoi(argv[a]);
+		a++;
 	}
 
 	printf("%d\n", total);
