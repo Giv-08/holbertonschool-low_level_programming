473ca86b136228929e28eb948a9f900aaad415cb
Giv-08 <chutima.p8@gmail.com>
2024-07-07 21:35:46 +0000

    added 0-create_array.c

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *create_array - prints buffer in hexa
+ * @c: Take char 'c' as parameter
+ * @size: Take int 'size' as parameter
+ * Return: NULL if size = 0
+ */
+
+void char *create_array(unsigned int size, char c)
+{
+	
+}
