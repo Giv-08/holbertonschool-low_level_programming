066e3d6004c2ec5679cb7d82edca1ca376aa3886
Giv-08 <chutima.p8@gmail.com>
2024-07-08 22:28:09 +0000

    added 2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,10 +14,9 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0, k = 0;
 	char *full_string;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
-		return ("");
-		_putchar(*s1);
+		s1 = "";
 	}
 
 	while (s1[i] != '\0')
