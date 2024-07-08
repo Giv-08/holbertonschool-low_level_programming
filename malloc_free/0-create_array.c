a5bc9eb064a8fa447216b5eabed44ff313a26fd8
Giv-08 <chutima.p8@gmail.com>
2024-07-08 01:55:45 +0000

    added 0-create_array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	char *array = (char *)malloc(size * sizeof(char));
 	unsigned int i = 0;
 
-	if (array == NULL || array == 0)
+	if (array == 0)
 	{
 		return (NULL);
 	}
