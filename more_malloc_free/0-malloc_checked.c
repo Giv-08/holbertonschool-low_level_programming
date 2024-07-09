b2a718b7f46458a892a2374a3f47ee9cbd9fc2a4
Giv-08 <chutima.p8@gmail.com>
2024-07-09 06:31:17 +0000

    added 0-malloc_checked

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,10 +1,23 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 
 /**
- * main - check the code
- *
+ * *malloc_checked - allocates memory using malloc
+ * @b : Take int 'b' as parameter
  * Return: Always 0.
  */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	else
+	{
+		return (ptr);
+	}
+}
