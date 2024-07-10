8ec024531ef74659c4c80590f2e9e960624370ea
Giv-08 <chutima.p8@gmail.com>
2024-07-10 04:21:28 +0000

    added dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,20 @@
-struct dog
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct Dog - Define a new type struct dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: Longer description of the struct if needed.
+ */
+
+struct Dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif /* DOG_H */
