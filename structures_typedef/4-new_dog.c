c789cb8a55a8c6a8f47de152173dbff20d69a14a
Giv-08 <chutima.p8@gmail.com>
2024-07-10 23:57:37 +0000

    added 4-new_dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * *new_dog - creates a new dog
+ * @name: Take name 'name' as parameter
+ * @age: Take float 'age' as parameter
+ * @owner: Take char 'owner' as parameter
+ * Return: NULL if the function fails
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+}
