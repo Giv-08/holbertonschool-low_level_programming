45d909765bfa35e3f50b4641fd65c38c47a2b2a0
Giv-08 <chutima.p8@gmail.com>
2024-07-11 23:13:35 +0000

    added 4-new_dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -37,17 +37,21 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
+	{
 		return (NULL);
-	free(new_dog);
+		free(new_dog);
+	}
 	new_dog->name = malloc(sizeof(dog_t));
 		if (new_dog->name == NULL)
 			return (NULL);
 	new_dog->name = _strdup(name);
 	new_dog->owner = malloc(sizeof(dog_t));
 		if (new_dog->owner == NULL)
+		{
 			free(new_dog->name);
 			free(new_dog);
 			return (NULL);
+		}
 	new_dog->owner = _strdup(owner);
 	new_dog->age = age;
 	return (new_dog);
