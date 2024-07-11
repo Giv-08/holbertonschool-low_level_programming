e32eeed5c487d385854769c53b9e8db2b16a2afe
Giv-08 <chutima.p8@gmail.com>
2024-07-11 04:53:20 +0000

    added 5-free_dog

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free the dogs
+ * @d - take dog_t 'd' as parameter
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d->age);
+	free(d);
+}
