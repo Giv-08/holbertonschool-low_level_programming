36783814da6a4c24be8f50cdb97900769688ade9
Giv-08 <chutima.p8@gmail.com>
2024-07-11 04:41:19 +0000

    added 4-new_dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * *new_dog - creates a new dog
@@ -10,51 +11,34 @@
  * Return: NULL if the function fails
  */
 
-int str_length(char *str)
-{
-	int length = 0;
-	while(str[length] != '\0')
-	{
-		length++;
-	}
-	return (length);
-}
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	unsigned int name_length, owner_length;
-	unsigned int i = 0;
 
-	new_dog = (char *)malloc(sizeof(dog_t));
+	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 	{
 		return (NULL);
 	}
 
-	name_length = str_length(name);
-	owner_length = str_length(owner);
+	new_dog->name = malloc(sizeof(dog_t));
+	{
+		if (new_dog->name == NULL)
+		{
+			return (NULL);
+		}
+	}
+	new_dog->name = strdup(name);
 
-	new_dog->name = (char *)malloc(name_length + 1);
-	if (new_dog->name == NULL)
+	new_dog->owner = malloc(sizeof(dog_t));
 	{
-		return (NULL);
+		if (new_dog->owner == NULL)
+		{
+			return (NULL);
+		}
 	}
-	while (i <= name_length)
-	{
-		new_dog->name[i] = name[i];
-		i++;
-	}
-	new_dog->owner = malloc(owner_length + 1);
-        if (new_dog->owner == NULL)
-        {
-                return (NULL);
-        }
-	while (i <= owner_length)
-        {
-                new_dog->owner[i] = owner[i];
-		i++;
-        }
+	new_dog->owner = strdup(owner);
+
 	new_dog->age = age;
 	return (new_dog);
 }
