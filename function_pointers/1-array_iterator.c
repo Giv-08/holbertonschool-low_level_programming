24d7f5752a7692add6470a54eb356c6daed1a9a8
Giv-08 <chutima.p8@gmail.com>
2024-07-15 01:33:53 +0000

    added 1-array_iterator

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_ierator - executes a function given
+ * as a parameter on each element of an array
+ * @array: name of the person
+ * @size: size of array
+ * @action: pointer function
+ * Return: Nothing.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	(void)size;
+	(action)(*array);
+}
