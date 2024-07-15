175a3b0e7ebb49ea077200857e899d0f5e86d404
Giv-08 <chutima.p8@gmail.com>
2024-07-15 05:00:51 +0000

    added 2-int_index

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -5,6 +5,7 @@
  * int_index - searches for an integer
  * @array: take int 'array' as parameter
  * @size: take int 'size' as paramter
+ * @cmp: function pointer take int as parameter
  * Return: If no element matches, return -1
  */
 
@@ -12,7 +13,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (size <= 0 || array == 0 )
+	if (size <= 0 || array == NULL )
 	{
 		return (-1);
 	}
