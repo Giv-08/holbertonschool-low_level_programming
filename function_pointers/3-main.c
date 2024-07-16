d4a7a9e3380b0d60056eb31625f4b45aeedd6898
Giv-08 <chutima.p8@gmail.com>
2024-07-16 09:42:03 +0000

    added main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -12,32 +12,32 @@
 
 int main(int argc, char *argv[])
 {
-	 int a, b, result;
-        char *operator;
-        int (*operation)(int, int);
+	int a, b, result;
+	char *operator;
+	int (*operation)(int, int);
 
-        if (argc != 4)
-        {
-                printf("Error\n");
-                return (98);
-        }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
 
-        a = atoi(argv[1]);
-        operator = argv[2];
-        b = atoi(argv[3]);
+	a = atoi(argv[1]);
+	operator = argv[2];
+	b = atoi(argv[3]);
 
-        if ((strlen(operator) == 1 && strchr("+-*/%", operator[0]) != NULL) &&
-        !((operator[0] == '/' || operator[0] == '%') && b == 0))
-        {
-                operation = get_op_func(operator);
-                if (operation != NULL)
-                {
-                        result = operation(a, b);
+	if ((strlen(operator) == 1 && strchr("+-*/%", operator[0]) != NULL) &&
+		!((operator[0] == '/' || operator[0] == '%') && b == 0))
+	{
+		operation = get_op_func(operator);
+		if (operation != NULL)
+		{
+			result = operation(a, b);
 			printf("%d\n", result);
-                        return (0);
-                }
-        }
+			return (0);
+		}
+	}
 
-        printf("Error\n");
-        return (operator[0] == '/' || operator[0] == '%') ? 100 : 99;
+	printf("Error\n");
+	return ((operator[0] == '/' || operator[0] == '%') ? 100 : 99);
 }
