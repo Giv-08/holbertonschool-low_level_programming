e5f495871b1b4b4663c88c52c575d1a6f89dc514
Giv-08 <chutima.p8@gmail.com>
2024-07-18 00:11:19 +0000

    added 3-print_all.c

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include "stdarg.h"
+
+void print_char(va_list *list)
+{
+	printf("%c", va_arg(*list, int));
+}
+void print_integer(va_list *list)
+{
+	printf("%d", va_arg(*list, int));
+}
+void print_float(va_list *list)
+{
+	printf("%f", va_arg(*list, double));
+}
+void print_str(va_list *list)
+{
+	char *data_type = va_arg(*list, char *);
+	if (data_type == NULL)
+	{
+		printf("(nil)");
+	}
+	else
+	{
+		printf("%s", data_type);
+	}
+}
+
+/**
+ * print_all - prints anything, followed by a new line
+ * @format: take any format as parameter
+ * Return: Always 0.
+ */
+
+void print_all(const char * const format, ...)
+{
+	int i = 0, j = 0;
+	char *separator;
+
+	fm fmt[] = {
+	{'c', print_char},
+	{'i', print_integer},
+	{'f', print_float},
+	{'s', print_str},
+	{'\0', NULL}
+	};
+
+	va_list lists;
+	va_start(lists, format);
+
+	while (format[i] != '\0')
+	{
+		while (fmt[j].data_type == format[i])
+		{
+			separator = ", ";
+			printf("%s", separator);
+			fmt[j].print(&lists);
+			break;
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(lists);
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,19 @@
+#include <stdarg.h>
+
 #ifndef VARIADIC_FUNCTIONS
 #define VARIADIC_FUNCTIONS
+
+/**
+ * struct op - Struct op
+ * @: The operator
+ * @f: The function associated
+ */
+
+typedef struct formatter
+{
+        char data_type;
+        void (*print)(va_list*);
+} fm;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
