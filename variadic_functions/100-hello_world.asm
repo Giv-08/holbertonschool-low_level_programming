5d1df8266f5d27165c05a2283251677ed8a131b3
Giv-08 <chutima.p8@gmail.com>
2024-07-21 04:08:40 +0000

    added a new line

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,18 +1,19 @@
 section .data
-    hello db 'Hello, world!', 0
+    hello db 'Hello, world!', 0x0A  ; 0x0A is the newline character
+    hello_len equ $ - hello          ; Length of the string including the newline
 
 section .text
     global _start
 
 _start:
     ; Write 'Hello, world!' to stdout
-    mov rax, 1          ; syscall number for sys_write
-    mov rdi, 1          ; file descriptor 1 is stdout
-    mov rsi, hello      ; address of string to output
-    mov rdx, 13         ; number of bytes
+    mov rax, 1              ; syscall number for sys_write
+    mov rdi, 1              ; file descriptor 1 is stdout
+    mov rsi, hello          ; address of string to output
+    mov rdx, hello_len      ; number of bytes
     syscall
 
     ; Exit
-    mov rax, 60         ; syscall number for sys_exit
-    xor rdi, rdi        ; exit code 0
+    mov rax, 60             ; syscall number for sys_exit
+    xor rdi, rdi            ; exit code 0
     syscall
