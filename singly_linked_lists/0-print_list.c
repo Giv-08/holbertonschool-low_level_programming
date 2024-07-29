73d2d31f77ee8756fc472603b663f6560da805cd
Giv-08 <chutima.p8@gmail.com>
2024-07-29 01:53:01 +0000

    added test and files

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ *
+ * Return: number of the nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	if (str == NULL)
+	{
+		_putchar
+	}
+}
