6b79f86fc9421e001e058c97a4af8ef79cc22c49
Giv-08 <chutima.p8@gmail.com>
2024-07-30 04:19:27 +0000

    added 3-add_node_end

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node_end - prints all the elements of a list_t list
+ * @head: take 'head' as argument
+ * @str: take 'str' as argument
+ * Return: the address of the new element,
+ * or NULL if it failed
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *tmp;
+	size_t length = 0;
+
+	new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+
+	new->str = strdup(str);
+	if (str[length])
+	{
+		length++;
+	}
+	new->len = length;
+	new->next = NULL;
+	tmp = *head;
+
+	if (tmp == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (tmp->next != NULL)
+		{
+			tmp = tmp->next;
+		}
+		tmp->next = new;
+	}
+	return (*head);
+}
