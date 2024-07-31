d81579bbfdfb7b8e6c96c3647308082e6b931bf2
Giv-08 <chutima.p8@gmail.com>
2024-07-31 01:41:14 +0000

    added 2-add_dnodeint

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -20,8 +20,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	}
 
 	new->n = n;
-	new->next = (*head);
-	(*head) = new;
+	new->next = *head;
+	new->prev = NULL;
+	*head = new;
 
 	return (new);
 }
