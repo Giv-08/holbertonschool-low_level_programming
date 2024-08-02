90a5aa04a16ea731a98d61afe7b9da51376e49e0
Giv-08 <chutima.p8@gmail.com>
2024-08-02 05:52:30 +0000

    added 5-flip_bits

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * clear_bit - returns the number of bits you would need to flip
+ * to get from one number to another
+ * @n: take 'n' as parameter
+ * @m: take 'm' as parameter
+ * Return: int count
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{	
+	unsigned long int result;
+	unsigned int count = 0;
+
+	result = n ^ m;
+
+	while (result)
+	{
+		count += result & 1;
+		result = result >> 1;
+	}
+	return (count);
+}
