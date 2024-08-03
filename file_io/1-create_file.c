79e19b85d7f2b6808926c93f36a7345d60315f51
Giv-08 <chutima.p8@gmail.com>
2024-08-03 04:49:21 +0000

    added 1-create_file.c

---
+++ file_io/1-create_file.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * create_textfile - creates a file
+ * @filename: take char 'filename' as parameter
+ * @text_content: take 'text_content' as parameter
+ * Return: 1 on success,if filename is NULL return -1
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t b_write;
+	int i = 0;
+	int file;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
+	if (file == -1)
+	{
+	
+		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		write(file, "", *text_content);
+	}
+	else
+	{
+		while (text_content[i] != '\0')
+		{
+			i++;
+		}
+		
+	}
+	b_write = write(file, text_content, i);
+	if (b_write == -1)
+	{
+		return (-1);
+	}
+
+	return (1);
+}
