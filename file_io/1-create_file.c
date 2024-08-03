2de4040f1f21830a66410eac31bc3403c2f09648
Giv-08 <chutima.p8@gmail.com>
2024-08-03 04:56:37 +0000

    added 1-create_file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -23,7 +23,6 @@ int create_file(const char *filename, char *text_content)
 	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (file == -1)
 	{
-	
 		return (-1);
 	}
 	if (text_content == NULL)
@@ -36,7 +35,6 @@ int create_file(const char *filename, char *text_content)
 		{
 			i++;
 		}
-		
 	}
 	b_write = write(file, text_content, i);
 	if (b_write == -1 || (int)b_write != i)
