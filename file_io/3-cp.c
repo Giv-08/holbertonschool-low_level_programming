a9bb8c3d3aa0f1f5be4e266f4fbe373b18dcf9d4
Giv-08 <chutima.p8@gmail.com>
2024-08-04 10:08:35 +0000

    added 3-cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -22,26 +22,29 @@ void err_exit(const char *str, const char *file, int code)
 
 int main(int argc, char *argv[])
 {
-        int file_from, file_to;
+        char *file_one, *file_two;
+	int file_from, file_to;
         int size = 1024;
         int b_write = 0;
         int *buffer[1024];
 
+	file_one = argv[1];
+	file_two = argv[2];
         if (argc != 3)
         {
                 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
                 exit(97);
         }
-        file_from = open(argv[1], O_RDONLY);
+        file_from = open(file_one, O_RDONLY);
         if (file_from == -1)
         {
-                err_exit("Error: Can't read from file %s\n", argv[1], 98);
+                err_exit("Error: Can't read from file %s\n", file_one, 98);
         }
 
-        file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        file_to = open(file_two, O_WRONLY | O_CREAT | O_TRUNC, 0644);
         if (file_to == -1)
         {
-                err_exit("Error: Can't write to %s\n", argv[2], 99);
+                err_exit("Error: Can't write to %s\n", file_two, 99);
         }
 
         while (size != 0)
@@ -49,18 +52,18 @@ int main(int argc, char *argv[])
                 size = read(file_from, buffer, 1024);
                 if (size < 0)
                 {
-                        err_exit("Error: Can't read from file %s\n", argv[1], 98);
+                        err_exit("Error: Can't read from file %s\n", file_one, 98);
                 }
 		b_write = write(file_to, buffer, size);
                 if (b_write < 0)
                 {
-			err_exit("Error: Can't write to %s\n", argv[2], 99);
+			err_exit("Error: Can't write to %s\n", file_two, 99);
                 }
         }
         if (close(file_from) == -1)
-                err_exit("Error: Can't close fd %d\n", argv[1], 100);
+                err_exit("Error: Can't close fd %d\n", file_one, 100);
 
         if (close(file_to) == -1)
-                err_exit("Error: Can't close fd %d\n", argv[2], 100);
+                err_exit("Error: Can't close fd %d\n", file_two, 100);
 	return (0);
 }
