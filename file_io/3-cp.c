bc2f59aaf432a775870cce35ee5253c8599c6c63
Giv-08 <chutima.p8@gmail.com>
2024-08-04 10:27:59 +0000

    added 3-cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,70 +1,63 @@
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 #include <unistd.h>
-
+#include <stdio.h>
 /**
- * main - check the code
- * @argc: no. of params
- * @argv: array of params
- *
- * Return: Always 0.
- */
+ * err_exit - function for dprintf
+ * @str: take char 'str' as parameter
+ * @file: take 'file' as parameter
+ * @code: take 'code' as parameter
+*/
+void err_exit(const char *str, const char *file, int code)
+{
+	dprintf(STDERR_FILENO, str, file);
+	exit(code);
+}
+/**
+ * main - main function
+ * @argc: take 'argc' as parameter
+ * @argv: take 'argv' as parameter
+ * Return: 0 on success
+*/
+
 int main(int argc, char *argv[])
 {
-	char *fone, *ftwo;
-	int fdone, fdtwo, sz = 1024, wr = 0, *buffer[1024];
+	int file_from, file_to;
+	int size = 1024;
+	int b_write = 0;
+	int *buffer[1024];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	fone = argv[1];
-	ftwo = argv[2];
-	fdone = open(fone, O_RDONLY);
-	fdtwo = open(ftwo, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-
-	if (fdone < 0)
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", fone);
-		exit(98);
+		err_exit("Error: Can't read from file %s\n", argv[1], 98);
 	}
-	if (fdtwo < 0)
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", ftwo);
-		exit(99);
+		err_exit("Error: Can't write to %s\n", argv[2], 99);
 	}
-
-	while (sz != 0)
+	while (size != 0)
 	{
-		sz = read(fdone, buffer, 1024);
-		if (sz < 0)
+		size = read(file_from, buffer, 1024);
+		if (size < 0)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", fone);
-			exit(98);
+			err_exit("Error: Can't read from file %s\n", argv[1], 98);
 		}
-		
-		wr = write(fdtwo, buffer, sz);
-		if (wr < 0)
+		b_write = write(file_to, buffer, size);
+		if (b_write < 0)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", ftwo);
-			exit(99);
+			err_exit("Error: Can't write to %s\n", argv[2], 99);
 		}
 	}
-
-	if (close(fdone) < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdone);
-		exit(100);
-	}
-
-	if (close(fdtwo) < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdtwo);
-		exit(100);
-	}
-        return (0);
+	if (close(file_from) == -1)
+		err_exit("Error: Can't close fd %d\n", argv[1], 100);
+	if (close(file_to) == -1)
+		err_exit("Error: Can't close fd %d\n", argv[2], 100);
+	return (0);
 }
