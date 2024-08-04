5ca9e7016f9f02b7202c3b7a095c52a53e515334
Giv-08 <chutima.p8@gmail.com>
2024-08-04 01:24:21 +0000

    added 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -19,49 +19,47 @@ void err_exit(char *str, char *file, int code)
  * @ft: take 'ft' as parameter
  * Return: 1 on success,if filename is NULL return -1
 */
+
 void _copy(char *ff, char *ft)
 {
-	int file_from, file_to;
-	ssize_t b_read, b_write;
-	char buffer[1024];
+    int file_from, file_to;
+    ssize_t b_read, b_write;
+    char buffer[1024];
 
-	file_from = open(ft, O_RDONLY);
-	if (file_from == -1)
-	{
-		err_exit("Error: Can't read from file %s\n", ff, 98);
-	}
-	file_to = open(ff, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (file_to == -1)
-	{
-		close(file_from);
-		err_exit("Error: Can't write from file %s\n", ft, 99);
-	}
+    file_from = open(ff, O_RDONLY);
+    if (file_from == -1)
+        err_exit("Error: Can't read from file %s\n", ff, 98);
 
-	b_read = read(file_from, buffer, sizeof(buffer));
-	while (b_read > 0)
-	{
-		b_write = write(file_to, buffer, b_read);
-		if (b_write != b_read)
-		{
-			close(file_from);
-			close(file_to);
-			err_exit("Error: Can't write to %s\n", ft, 99);
-		}
-	}
-	if (b_read == -1)
-	{
-		close(file_from);
-		close(file_to);
-		err_exit("Error: Can't read to %s\n", ff, 98);
-	}
-	if (close(file_from) == -1)
-	{
-		err_exit("Error: Can't close fd %d\n", ff, 100);
-	}
-	if (close(file_to) == -1)
-	{
-		err_exit("Error: Can't close fd %d\n", ft, 100);
-	}
+    file_to = open(ft, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (file_to == -1)
+    {
+        close(file_from);
+        err_exit("Error: Can't write to %s\n", ft, 99);
+    }
+
+    while ((b_read = read(file_from, buffer, sizeof(buffer))) > 0)
+    {
+        b_write = write(file_to, buffer, b_read);
+        if (b_write != b_read)
+        {
+            close(file_from);
+            close(file_to);
+            err_exit("Error: Can't write to %s\n", ft, 99);
+        }
+    }
+
+    if (b_read == -1)
+    {
+        close(file_from);
+        close(file_to);
+        err_exit("Error: Can't read from %s\n", ff, 98);
+    }
+
+    if (close(file_from) == -1)
+        err_exit("Error: Can't close fd %d\n", ff, 100);
+
+    if (close(file_to) == -1)
+        err_exit("Error: Can't close fd %d\n", ft, 100);
 }
 /**
  * main - main function
