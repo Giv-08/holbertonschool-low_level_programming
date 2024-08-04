873b0ebeef01ef138d65618d83c7ebc98afc5c6f
Giv-08 <chutima.p8@gmail.com>
2024-08-04 00:52:24 +0000

    added 3-cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -27,26 +27,38 @@ void _copy(char *ff, char *ft)
 
 	file_to = open(ff, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (file_to == -1)
+	{
 		err_exit("Error: Can't write from file %s\n", ft, 99);
+	}
 	file_from = open(ft, O_RDONLY);
 	if (file_from == -1)
+	{
 		err_exit("Error: Can't read from file %s\n", ff, 98);
+	}
+
 	b_write = write(file_to, buffer, 100);
 	if (b_write == -1)
 	{
 		err_exit("Error: Can't write to %s\n", ft, 99);
 	}
-	b_read = read(file_from, buffer, 100);
-	while ((b_read = read(file_from, buffer, sizeof(buffer))) > 0)
+
+	b_read = read(file_from, buffer, sizeof(buffer));
+	while (b_read > 0)
 	{
 		b_write = write(file_to, buffer, b_read);
 		if (b_write == -1)
+		{
 			err_exit("Error: Can't write to %s\n", ft, 99);
+		}
 	}
 	if (b_read == -1)
+	{
 		err_exit("Error: Can't read to %s\n", ff, 98);
+	}
 	if (close(file_from) == -1 || close(file_to) == -1)
+	{
 		err_exit("Error: Can't close fd %d\n", ff, 100);
+	}
 }
 /**
  * main - main function
