c5fe1ef8dc6ac14cfb8f972ecdee347bcff1f6ef
Giv-08 <chutima.p8@gmail.com>
2024-08-04 01:14:36 +0000

    added 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -25,17 +25,17 @@ void _copy(char *ff, char *ft)
 	ssize_t b_read, b_write;
 	char buffer[1024];
 
-	file_to = open(ff, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (file_to == -1)
-	{
-		err_exit("Error: Can't write from file %s\n", ft, 99);
-	}
 	file_from = open(ft, O_RDONLY);
 	if (file_from == -1)
 	{
 		err_exit("Error: Can't read from file %s\n", ff, 98);
 	}
-
+	file_to = open(ff, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (file_to == -1)
+	{
+		close(file_from);
+		err_exit("Error: Can't write from file %s\n", ft, 99);
+	}
 	b_write = write(file_to, buffer, 100);
 	if (b_write == -1)
 	{
@@ -46,18 +46,26 @@ void _copy(char *ff, char *ft)
 	while (b_read > 0)
 	{
 		b_write = write(file_to, buffer, b_read);
-		if (b_write == -1)
+		if (b_write != b_read)
 		{
+			close(file_from);
+			close(file_to);
 			err_exit("Error: Can't write to %s\n", ft, 99);
 		}
 	}
 	if (b_read == -1)
 	{
+		close(file_from);
+		close(file_to);
 		err_exit("Error: Can't read to %s\n", ff, 98);
 	}
-	if (close(file_from) == -1 || close(file_to) == -1)
+	if (close(file_from) == -1)
 	{
 		err_exit("Error: Can't close fd %d\n", ff, 100);
+	}
+	if (close(file_to) == -1)
+	{
+		err_exit("Error: Can't close fd %d\n", ft, 100);
 	}
 }
 /**
