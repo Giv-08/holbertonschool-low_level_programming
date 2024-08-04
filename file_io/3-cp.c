3da64878147bb4d9d161eaf626fe1868fd503188
Giv-08 <chutima.p8@gmail.com>
2024-08-04 00:46:14 +0000

    added 3-cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -2,21 +2,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
-int main(int argc, char *argv[])
+/**
+ * err_exit - function for dprintf
+ * @str: take char 'str' as parameter
+ * @file: take 'file' as parameter
+ * @code: take 'code' as parameter
+*/
+void err_exit(char *str, char *file, int code)
 {
-    if (argc != 3)
-    {
-        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-        exit(97);
-    }
-    _copy(argv[1], argv[2]);
-    return (0);
-}
-
-void err_exit (char *str, char *file, int code)
-{
-        dprintf(STDERR_FILENO, str, file);
-        exit(code);
+	dprintf(STDERR_FILENO, str, file);
+	exit(code);
 }
 /**
  * _copy - copy content from one file to another
@@ -52,4 +47,20 @@ void _copy(char *ff, char *ft)
 		err_exit("Error: Can't read to %s\n", ff, 98);
 	if (close(file_from) == -1 || close(file_to) == -1)
 		err_exit("Error: Can't close fd %d\n", ff, 100);
+}
+/**
+ * main - main function
+ * @argc: take int 'argc' as parameter
+ * @argv: take char 'argv' as parameter
+ * Return: 0 on success
+*/
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	_copy(argv[1], argv[2]);
+	return (0);
 }
