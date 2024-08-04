54d84c301b5896bf49d93ff7e1b1497ab8d8b4a1
Giv-08 <chutima.p8@gmail.com>
2024-08-04 10:05:10 +0000

    added 3-cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -20,23 +20,28 @@ void err_exit(const char *str, const char *file, int code)
  * Return: 1 on success,if filename is NULL return -1
 */
 
-void _copy(const char *ff, const char *ft)
+int main(int argc, char *argv[])
 {
         int file_from, file_to;
         int size = 1024;
         int b_write = 0;
         int *buffer[1024];
 
-        file_from = open(ff, O_RDONLY);
+        if (argc != 3)
+        {
+                dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+                exit(97);
+        }
+        file_from = open(argv[1], O_RDONLY);
         if (file_from == -1)
         {
-                err_exit("Error: Can't read from file %s\n", ff, 98);
+                err_exit("Error: Can't read from file %s\n", argv[1], 98);
         }
 
-        file_to = open(ft, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
         if (file_to == -1)
         {
-                err_exit("Error: Can't write to %s\n", ft, 99);
+                err_exit("Error: Can't write to %s\n", argv[2], 99);
         }
 
         while (size != 0)
@@ -44,33 +49,18 @@ void _copy(const char *ff, const char *ft)
                 size = read(file_from, buffer, 1024);
                 if (size < 0)
                 {
-                        err_exit("Error: Can't read from file %s\n", ff, 98);
+                        err_exit("Error: Can't read from file %s\n", argv[1], 98);
                 }
 		b_write = write(file_to, buffer, size);
                 if (b_write < 0)
                 {
-			err_exit("Error: Can't write to %s\n", ft, 99);
+			err_exit("Error: Can't write to %s\n", argv[2], 99);
                 }
         }
         if (close(file_from) == -1)
-                err_exit("Error: Can't close fd %d\n", ff, 100);
+                err_exit("Error: Can't close fd %d\n", argv[1], 100);
 
         if (close(file_to) == -1)
-                err_exit("Error: Can't close fd %d\n", ft, 100);
-}
-/**
- * main - main function
- * @argc: take int 'argc' as parameter
- * @argv: take char 'argv' as parameter
- * Return: 0 on success
-*/
-int main(int argc, char *argv[])
-{
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	_copy(argv[1], argv[2]);
+                err_exit("Error: Can't close fd %d\n", argv[2], 100);
 	return (0);
 }
