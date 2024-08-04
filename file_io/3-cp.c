be4fc72a944d69ef51698dc5c2eed8582453ab7f
Giv-08 <chutima.p8@gmail.com>
2024-08-04 01:53:33 +0000

    added 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,81 +1,81 @@
-#include <stdio.h>
 #include "main.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 /**
- * _error - Read file.
- * @e: Error number
- * @filename: File name
- */
-void _error(int e, char *filename)
+ * err_exit - function for dprintf
+ * @str: take char 'str' as parameter
+ * @file: take 'file' as parameter
+ * @code: take 'code' as parameter
+*/
+void err_exit(const char *str, const char *file, int code)
 {
-	if (e == 98)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-		exit(98);
-	}
-	if (e == 99)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-		exit(99);
-	}
+	dprintf(STDERR_FILENO, str, file);
+	exit(code);
 }
 /**
- * cp - Copies the content of a file to another file.
- * @file_from: Name of the source file.
- * @file_to: Name of the destination file.
- * Return: 1 on success, -1 on failure.
- */
-void cp(char *file_from, char *file_to)
-{
-	int fd_read, res_read, fd_write, res_write;
-	char *buf[1024];
+ * _copy - copy content from one file to another
+ * @ff: take char 'ff' as parameter
+ * @ft: take 'ft' as parameter
+ * Return: 1 on success,if filename is NULL return -1
+*/
 
-	fd_read = open(file_from, O_RDONLY);
-	if (fd_read < 0)
-		_error(98, file_from);
-	fd_write = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd_write < 0)
+void _copy(const char *ff, const char *ft)
+{
+	int file_from, file_to;
+	ssize_t b_read, b_write;
+	char buffer[1024];
+
+	file_from = open(ff, O_RDONLY);
+	if (file_from == -1)
 	{
-		close(fd_read);
-		_error(99, file_to);
+		err_exit("Error: Can't read from file %s\n", ff, 98);
 	}
-	do {
-		res_read = read(fd_read, buf, 1024);
-		if (res_read < 0)
-			_error(98, file_from);
-		res_write = write(fd_write, buf, res_read);
-		if (res_write < res_read)
-			_error(99, file_to);
-	}	while (res_write == 1024);
-	if (close(fd_read) < 0)
+
+	file_to = open(ft, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_read);
-		close(fd_write);
-		exit(100);
+		close(file_from);
+		err_exit("Error: Can't write to %s\n", ft, 99);
 	}
-	if (close(fd_write) < 0)
+
+	while ((b_read = read(file_from, buffer, sizeof(buffer))) > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_write);
-		exit(100);
+		b_write = write(file_to, buffer, b_read);
+		if (b_write != b_read)
+		{
+			close(file_from);
+			close(file_to);
+			err_exit("Error: Can't write to %s\n", ft, 99);
+		}
 	}
+
+	if (b_read == -1)
+	{
+		close(file_from);
+		close(file_to);
+		err_exit("Error: Can't read from %s\n", ff, 98);
+	}
+
+	if (close(file_from) == -1)
+		err_exit("Error: Can't close fd %d\n", ff, 100);
+
+	if (close(file_to) == -1)
+		err_exit("Error: Can't close fd %d\n", ft, 100);
 }
 /**
- * main - Copies the content of a file to another file.
- * @ac: Argument count
- * @av: argument values
- * Return: 0 on succes, -1 on error.
- */
-int main(int ac, char *av[])
+ * main - main function
+ * @argc: take int 'argc' as parameter
+ * @argv: take char 'argv' as parameter
+ * Return: 0 on success
+*/
+int main(int argc, char *argv[])
 {
-	if (ac != 3)
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	cp(av[1], av[2]);
+	_copy(argv[1], argv[2]);
 	return (0);
 }
