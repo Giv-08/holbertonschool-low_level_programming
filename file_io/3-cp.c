c0329f690fda2aa684b3934b9c829c4dee5ccc00
Giv-08 <chutima.p8@gmail.com>
2024-08-05 10:51:35 +0000

    added 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -39,15 +39,22 @@ int main(int argc, char *argv[])
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
+		close(file_from);
 		err_exit("Error: Can't write to %s\n", argv[2], 99);
 	}
 	while ((size = read(file_from, buffer, sizeof(buffer))) > 0)
 	{
 		if (write(file_to, buffer, size) != size)
+		{
+			close(file_from);
+			close(file_to);
 			err_exit("Error: Can't write to %s\n", argv[2], 99);
+		}
 	}
 	if (size == -1)
 	{
+		close(file_from);
+		close(file_to);
 		err_exit("Error: Can't read from file %s\n", argv[1], 98);
 	}
 	if (close(file_from) == -1)
